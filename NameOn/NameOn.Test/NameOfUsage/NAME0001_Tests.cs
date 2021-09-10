a50ae5c2c88514f06f5da1ff9a3e7d8184ece6e9
Alex <kalfas.alex@gmail.com>
2021-09-10 20:46:20 +0300

    Add more tests
    That should be implemented some day

--- NameOn/NameOn.Test/NameOfUsage/NAME0001_Tests.cs
+++ NameOn/NameOn.Test/NameOfUsage/NAME0001_Tests.cs
@@ -65,5 +65,133 @@ class Program
 
             AssertDiagnosticsWithUsings(testCode);
         }
+
+        [TestMethod]
+        public void DelegateInvocationArgumentNameOf()
+        {
+            var testCode =
+@"
+class Program
+{
+    void Run(Function f)
+    {
+        f(nameof(Run));
+        f(↓""Run"");
+    }
+
+    delegate void Function([ForceNameOf] string name);
+}
+";
+
+            AssertDiagnosticsWithUsings(testCode);
+        }
+
+        [TestMethod]
+        public void ConstructorAssignmentNameOf()
+        {
+            var testCode =
+@"
+class C
+{
+    [ForceNameOf]
+    string Value { get; set; }
+
+    public C(string value)
+    {
+        Value = ↓value;
+    }
+    public C([ForceNameOf] string value, bool dummy)
+    {
+        Value = value;
+    }
+}
+";
+
+            AssertDiagnosticsWithUsings(testCode);
+        }
+
+        [TestMethod]
+        public void ConstructorInvocationNameOf()
+        {
+            var testCode =
+@"
+class Program
+{
+    void Function()
+    {
+        new C(↓""value"");
+        new C(nameof(C));
+    }
+}
+
+class C
+{
+    public C([ForceNameOf] string value)
+    {
+    }
+}
+";
+
+            AssertDiagnosticsWithUsings(testCode);
+        }
+
+        [TestMethod]
+        public void FunctionReturnNameOf()
+        {
+            var testCode =
+@"
+class Program
+{
+    [return: ForceNameOf]
+    string Function()
+    {
+        return ↓""value"";
+    }
+    [return: ForceNameOf]
+    string AnotherFunction()
+    {
+        return nameof(AnotherFunction);
+    }
+}
+";
+
+            AssertDiagnosticsWithUsings(testCode);
+        }
+
+        [TestMethod]
+        public void DelegateReturnNameOf()
+        {
+            var testCode =
+@"
+class Program
+{
+    void Function()
+    {
+        NameGetter getter = ↓GetRandomString;
+        NameGetter anotherGetter = GetName;
+
+        var name = anotherGetter();
+        AcceptName(name);
+    }
+    
+    void AcceptName([ForceNameOf] string name) { }
+
+    string GetRandomString()
+    {
+        return ""random"";
+    }
+    [return: ForceNameOf]
+    string GetName()
+    {
+        return nameof(NameGetter);
+    }
+
+    [return: ForceNameOf]
+    delegate string NameGetter();
+}
+";
+
+            AssertDiagnosticsWithUsings(testCode);
+        }
     }
 }
